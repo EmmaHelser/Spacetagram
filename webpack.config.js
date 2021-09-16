8da65a9238522b70d4c5838f9ec08c0d4cfbd11b
Emma <77553206+EmmaHelser@users.noreply.github.com>
2021-09-16 09:22:38 -0700

    Installing dependencies, configuring webpack and babel, creating skeleton for app.

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/src',
+  rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    },
+  ],
+  output: {
+    path: path.resolve(__dirname, './client/dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
