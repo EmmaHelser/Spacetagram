419c86f0e13098dba05886d40e6978da8338ee9f
Emma <77553206+EmmaHelser@users.noreply.github.com>
2021-09-16 10:34:48 -0700

    Created basic layout for image card to be displayed for spacetagram. Image card includes image, title, description, date, and like symbol place holders.

---
+++ client/src/components/imageCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ImageCard (props) {
+  return (
+    <div className='container'>
+      <h2>Image</h2>
+      <h3>Image Title</h3>
+      <p>Description</p>
+      <div>
+        <h4>Date</h4>
+        <h4>Like</h4>
+      </div>
+    </div>
+  )
+}
+
+export default ImageCard;
