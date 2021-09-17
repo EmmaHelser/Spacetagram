a01e60c3a088975ee30206d71b3ee66b3f225c03
Emma <77553206+EmmaHelser@users.noreply.github.com>
2021-09-17 10:43:05 -0700

    Added api request to main app file, set state to hold retrieved images list and to retrieve a list of liked photos from local storage. Passing images list and liked photos list down to sub components.

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import ImageList from './imageList.jsx';
+import token from '../../../token.js';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-      images: [{image: 'rocket image', title: 'rocket', description: 'a rocket', date: '01/22/22', liked: false}, {image: 'rocket image', title: 'rocket', description: 'a rocket', date: '01/22/22', liked: false}, {image: 'rocket image', title: 'rocket', description: 'a rocket', date: '01/22/22', liked: false}]
+      images: [],
+      likedPhotos: []
     }
+  }
+
+  componentDidMount () {
+
+    const likedPhotos = localStorage.favSpacePcs ? JSON.parse(localStorage.faveSpacePics) : this.state.likedPhotos;
+
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${token.token}&count=10`)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({images: response.data, likedPhotos: likedPhotos});
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
 
   render () {
     return (
       <div className='container'>
         <h1>Spacetagram</h1>
-        <ImageList images={this.state.images}/>
+        <ImageList images={this.state.images} liked={this.state.likedPhotos} />
       </div>
 
     )
