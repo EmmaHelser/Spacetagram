5cff0e3a47bab2d6819c96a942b062c3efb1b8f6
Emma <77553206+EmmaHelser@users.noreply.github.com>
2021-09-17 11:23:07 -0700

    Added conditional statement to display an empty heart when image not liked and red heart when image liked.

--- client/src/components/imageCard.jsx
+++ client/src/components/imageCard.jsx
@@ -4,11 +4,14 @@ function ImageCard (props) {
   return (
     <div className='cardContainer'>
       <img src={props.image.url}></img>
-      <h3>{props.image.title}</h3>
-      <p>{props.image.explanation}</p>
-      <div>
+      <h3 className='imageTitle'>{props.image.title}</h3>
+      <p className='ImageDescription'>{props.image.explanation}</p>
+      <div className='dateAndLiked'>
         <h4>{props.image.date}</h4>
-        <h4>{props.liked === true ? 'liked' : 'not liked'}</h4>
+        {props.liked === true ?
+        <h4>&#10084;</h4> :
+        <h4>&#9825;</h4>
+        }
       </div>
     </div>
   )
