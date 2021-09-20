465785b4d8c792433caa93809e6f11b42228478b
Emma <77553206+EmmaHelser@users.noreply.github.com>
2021-09-20 10:10:11 -0700

    Added alt attributes to all elements to improve accessibilty of site.

--- client/src/components/imageCard.jsx
+++ client/src/components/imageCard.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 function ImageCard (props) {
   return (
     <div className='cardContainer'>
-      <img src={props.image.url}></img>
-      <h3 className='imageTitle'>{props.image.title}</h3>
-      <p className='ImageDescription'>{props.image.explanation}</p>
+      <img src={props.image.url} alt={`${props.image.title}`}></img>
+      <h3 className='imageTitle' alt=''>{props.image.title}</h3>
+      <p className='imageDescription' alt={`${props.image.explanation}`}>{props.image.explanation}</p>
       <div className='dateAndLiked'>
-        <h4>{props.image.date}</h4>
+        <h4 className='date' alt={`Date taken: ${props.image.date}`}>{props.image.date}</h4>
         {props.liked === true ?
-        <h4 onClick={() => {props.unlikeImage(props.image.title)}}>&#10084;</h4> :
-        <h4 onClick={() => {props.imageLiked(props.image.title)}}>&#9825;</h4>
+        <h4 className='liked' onClick={() => {props.unlikeImage(props.image.title)}} alt='Image liked'>&#10084;</h4> :
+        <h4 className='liked' onClick={() => {props.imageLiked(props.image.title)}} alt='Image not liked'>&#9825;</h4>
         }
       </div>
     </div>
