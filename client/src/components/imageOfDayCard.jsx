1528d5c6c5061a6b34a8cecd8e7057216c27f2bb
Emma <77553206+EmmaHelser@users.noreply.github.com>
2021-09-20 17:26:03 -0700

    Added an image of the day card to load first and then load all other images.

---
+++ client/src/components/imageOfDayCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function ImageOfTheDayCard (props) {
+  return (
+    <div className='cardContainer'>
+      <h2 alt='Image of the Day' className='imageOfTheDay'>Image of the Day</h2>
+      <img src={props.image.url} alt={`${props.image.title}`}></img>
+      <h3 className='imageTitle' alt=''>{props.image.title}</h3>
+      <p className='imageDescription' alt={`${props.image.explanation}`}>{props.image.explanation}</p>
+      <div className='dateAndLiked'>
+        <h4 className='date' alt={`Date taken: ${props.image.date}`}>{props.image.date}</h4>
+        {props.liked === true ?
+        <h4 className='liked' onClick={() => {props.unlikeImage(props.image.title)}} alt='Image liked'>&#10084;</h4> :
+        <h4 className='liked' onClick={() => {props.imageLiked(props.image.title)}} alt='Image not liked'>&#9825;</h4>
+        }
+      </div>
+    </div>
+  )
+}
+
+export default ImageOfTheDayCard;
