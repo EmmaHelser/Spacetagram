58883a41bd594825c7241063dc4c8989e58c3533
Emma <77553206+EmmaHelser@users.noreply.github.com>
2021-09-20 17:25:00 -0700

    Adjusting imageList to control for an empty list of images.

--- client/src/components/imageList.jsx
+++ client/src/components/imageList.jsx
@@ -4,7 +4,7 @@ import ImageCard from './imageCard.jsx';
 function ImageList (props) {
   return (
     <div className='listContainer'>
-      {props.images.map((photo, index) => <ImageCard image={photo} key={index} liked={props.liked.includes(photo.title)} imageLiked={props.imageLiked} unlikeImage={props.unlikeImage} /> )}
+      {props.images.map((photo, index) => <ImageCard image={photo} key={index} liked={props.liked !== [] ? props.liked.includes(photo.title) : false} imageLiked={props.imageLiked} unlikeImage={props.unlikeImage} /> )}
     </div>
   )
 }
