8b5a92a099b12a6e63fd51210ea5651c51ffc28e
Emma <77553206+EmmaHelser@users.noreply.github.com>
2021-09-20 17:33:47 -0700

    Added like and unlike functionality to image of the day card, refactored api calls to grab less pictures at a time.

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,21 +2,20 @@ import React, {useState, useEffect} from 'react';
 import ImageList from './imageList.jsx';
 import token from '../../../token.js';
 import axios from 'axios';
+import ImageOfTheDayCard from './imageOfDayCard.jsx';
 
 function App (props) {
   const [images, setImages] = useState(null);
-  const [likedPhotos, setLikedPhotos] = useState([]);
+  const [likedPhotos, setLikedPhotos] = useState('');
+  const [imageOfDay, setImageOfDay] = useState(null);
+
 
   useEffect(() => {
-    const date = new Date();
-    const currentDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`;
-    const pastDate = date.getDay() - 15 > 0 ? `${date.getFullYear()}-${date.getMonth()}-${date.getDay() - 15}` : `${date.getFullYear()}-${date.getMonth() - 1}-${20}`;
-    const likedPhotos = localStorage.favSpacePics ? JSON.parse(localStorage.favSpacePics) : likedPhotos;
-
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${token.token}&start_date=${pastDate}&end_date=${currentDate}`)
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${token.token}`)
       .then((response) => {
-        let images = response.data.reverse();
-        setImages(images);
+        let images = response.data;
+
+        setImageOfDay(images);
         setLikedPhotos(likedPhotos);
       })
       .catch((err) => {
@@ -25,9 +24,25 @@ function App (props) {
   }, []);
 
   useEffect(() => {
-    const date = new Date();
-    const currentDate = date.getDay() - 15 > 0 ? `${date.getFullYear()}-${date.getMonth()}-${date.getDay() - 15}` : `${date.getFullYear()}-${date.getMonth() - 1}-${20}`;
-  }, [images]);
+    const date = imageOfDay ? imageOfDay.date : new Date();
+    const dateParts = typeof date === 'string' ? date.split('-') : null;
+    const day = dateParts ? Number.parseInt(dateParts[2]) : null;
+    const currentDate = dateParts ? `${dateParts[0]}-${dateParts[1]}-${day - 1}` : null;
+    const pastDate = dateParts ? `${dateParts[0]}-${Number.parseInt(dateParts[1]) - 1}-${day - 15 > 0 ? day - 15 : 20}` : null;
+    const likedPhotos = localStorage.favSpacePics ? JSON.parse(localStorage.favSpacePics) : likedPhotos;
+
+    if (typeof date === 'string') {
+      axios.get(`https://api.nasa.gov/planetary/apod?api_key=${token.token}&start_date=${pastDate}&end_date=${currentDate}`)
+      .then((response) => {
+        let images = response.data.reverse();
+        setImages(images);
+        setLikedPhotos(likedPhotos);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    }
+  }, [imageOfDay]);
 
   const imageLiked = (imageTitle) => {
     const likedImages = localStorage.favSpacePics ? JSON.parse(localStorage.favSpacePics) : [];
@@ -48,7 +63,7 @@ function App (props) {
     localStorage.setItem('favSpacePics', JSON.stringify(likedImages));
   }
 
-    if (images === null) {
+    if (imageOfDay === null) {
       return (
         <div className='container'>
           <div className="lds-dual-ring"></div>
@@ -59,7 +74,12 @@ function App (props) {
       return (
         <div className='container'>
           <h1 className='appName' alt='Spacetagram'>Spacetagram</h1>
-          <ImageList images={images} liked={likedPhotos} imageLiked={imageLiked}  unlikeImage={unlikeImage} />
+          <p className='credit' alt='Brought to you by NASA Images API'>Brought to you by NASA Images API</p>
+          <ImageOfTheDayCard image={imageOfDay} imageLiked={imageLiked}  unlikeImage={unlikeImage} />
+          { images ?
+              <ImageList images={images} imageOfDay={imageOfDay} liked={likedPhotos} imageLiked={imageLiked}  unlikeImage={unlikeImage} /> :
+              <div className="lds-dual-ring"></div>
+          }
         </div>
       )
     }
